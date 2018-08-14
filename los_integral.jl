149b1ac123d72aa01c15253b4289e440044817e7
d45fg4 <sdh0618@users.noreply.github.com>
2018-08-14 17:22:31 -0400

    under developing

---
+++ los_integral.jl
@@ -0,0 +1,31 @@
+Using Distributed
+
+function nuclear_bulge(d,l,b)
+	 z = d*sin(b*d2r)
+	 x = Rsun - d*cos(b*d2r)*cos(l*d2r)
+	 y = -d*cos(b*d2r)*sin(l*d2r)
+	 R = sqrt(Rsun^2-2*d*Rsun*cos(b*d2r)*cos(l*d2r)+d^2)
+	 r = sqrt(Rsun^2-2*d*Rsun*cos(b*d2r)*cos(l*d2r)+(d*cos(b*d2r))^2
+	 if R < 0.006
+                  nsc = 3.3e+15/(1+(R/2.2e-4)^2)
+         else-if 0.006 <= R <= 0.23
+                  nsc = 27.2375*3.3e+15/(1+(R/2.2e-4)^3)
+         else
+                  nsc = 0
+         if r < 0.12
+                  nsd = 3e+11*(1000*r)^(-0.1)*exp(-abs(z)/0.045))
+         else-if 0.12 <= r < 0.22
+		  nsd = 1.17278e+7*3e+11*(1000*r)^(-3.5)*exp(-abs(z)/0.045))
+	 else-if 0.22 <= r < 0.23
+		  nsd = 1.97226e+22*3e+11*(1000*r)^(-10)*exp(-abs(z)/0.045))
+	 else
+		  nsd = 0
+
+	 return nsc + nsd
+
+function los(M)
+	anglos =  @distributed (+) for d = 8:0.01:8.5
+		nuclear_bulge(d,M[1],M[2])	
+
+A = [ [b,l] for b=0:0.5:179.5, l=0:0.5:359.5 ]
+
